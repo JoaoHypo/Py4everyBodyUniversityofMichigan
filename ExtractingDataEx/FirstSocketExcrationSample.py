810808a04496d51998d04c534a1a9a655ace2b6a
Hypo - Unb <jguibarboza@gmail.com>
2023-07-05 22:54:44 -0300

    class revision

--- ExtractingDataEx/FirstSocketExcrationSample.py
+++ ExtractingDataEx/FirstSocketExcrationSample.py
@@ -4,7 +4,7 @@ mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('data.pr4e.org', 80))
 cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
-
+# its obligated to have \r b4 \n the combo \r\n is the same as ENTER
 while True:
     data = mysock.recv(512)
     if len(data) < 1:
