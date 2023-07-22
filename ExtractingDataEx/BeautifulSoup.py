72b083fb73b167f50cfa38d25b67ab492fb6a489
JoaoHypo <jguibarboza@gmail.com>
2023-07-22 20:44:12 -0300

    1

--- ExtractingDataEx/BeautifulSoup.py
+++ ExtractingDataEx/BeautifulSoup.py
@@ -1,9 +1,9 @@
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 
-handle = urllib.request.urlopen('http://data.pr4e.org/intro-short.txt')
-#access the page with a handle like an open command.
+url = input("Enter the URL -") # Asking for the URL
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parse')
 
-for line in handle:
-    print(line.decode().strip())
-    #.decode() transforma o texto do type bytes, provavel em utf-8 para string do py
+print(soup)
+#Retrive all of the ancher tags
