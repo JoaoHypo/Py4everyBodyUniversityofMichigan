555427ef4b2ed4a9ed9768c73ae63bc080d13fe2
JoaoHypo <jguibarboza@gmail.com>
2023-07-22 22:16:15 -0300

    print done

--- ExtractingDataEx/BeautifulSoup.py
+++ ExtractingDataEx/BeautifulSoup.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 url = input("Enter the URL -") # Asking for the URL
 html = urllib.request.urlopen(url).read()
-soup = BeautifulSoup(html, 'html.parse')
+soup = BeautifulSoup(html, 'html.parser')
 
 print(soup)
 #Retrive all of the ancher tags
