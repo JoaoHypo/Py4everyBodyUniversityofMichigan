26fc022e7a241b9c4fbe0e33a08b0d4ebaa64d23
Hypo - Unb <jguibarboza@gmail.com>
2023-07-23 20:16:10 -0300

    XML parsing tag with multiple children

---
+++ ExtractingDataEx/XMLparsingMultipleTag.py
@@ -0,0 +1,25 @@
+import xml.etree.ElementTree as ET
+
+data = '''<stuff>
+    <users>
+        <user x="2">
+            <id>001</id>
+            <name>Hypo</name>
+        </user>
+        <user x="7">
+            <id>009</id>
+            <name>Bru</name>
+        </user>
+    </users>
+</stuff>'''
+
+tree = ET.fromstring(data)
+
+lst = tree.findall('users/user')
+print("User count:", len(lst))
+print()
+for i in lst:
+    print("Name:", i.find('name').text)
+    print("ID:", i.find('id').text)
+    print("Attribute:", i.get("x"))
+    print()
