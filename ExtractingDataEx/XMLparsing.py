3915b22ade1b4af8996f977ab6745a32dbd2df07
Hypo - Unb <jguibarboza@gmail.com>
2023-07-23 19:59:23 -0300

    Parsing SIngle Tag

--- ExtractingDataEx/XMLparsing.py
+++ ExtractingDataEx/XMLparsing.py
@@ -9,7 +9,13 @@ data = '''<person>
 </person>'''
 
 tree = ET.fromstring(data)
+# this can blow up if data has error
+
 print(tree)
 print(type(tree.find("name")))
 print("Name: ",tree.find("name").text)
+#extracts the text from the tag
+
+
 print("Attr: ",tree.find("email").get("hide"))
+#extracts the atribute value, text does not exist
