3d0ac63c45468ce7e0760f54174e957c1113388a
JoaoHypo <jguibarboza@gmail.com>
2023-07-22 22:31:13 -0300

    Added SSL error fix

--- ExtractingDataEx/BeautifulSoup.py
+++ ExtractingDataEx/BeautifulSoup.py
@@ -1,8 +1,14 @@
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
+import ssl
+
+#Ignoring SSL errors:
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 url = input("Enter the URL -") # Asking for the URL
-html = urllib.request.urlopen(url).read()
+html = urllib.request.urlopen(url,context=ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
 
 #print(soup)
