b83faabb2c09da75c5d11e131b5bfcc1a711b051
JoaoHypo <jguibarboza@gmail.com>
2023-07-22 22:25:24 -0300

    more tests

--- ExtractingDataEx/BeautifulSoup.py
+++ ExtractingDataEx/BeautifulSoup.py
@@ -5,5 +5,10 @@ url = input("Enter the URL -") # Asking for the URL
 html = urllib.request.urlopen(url).read()
 soup = BeautifulSoup(html, 'html.parser')
 
-print(soup)
+#print(soup)
 #Retrive all of the ancher tags
+tags = soup('a') #getting the anchors of the HTML and returning a list with the anchors
+#this typo its called tag.
+for tag in tags:
+    #print(type(tag))
+    print(tag.get('href',None))
