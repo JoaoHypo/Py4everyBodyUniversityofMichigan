107b0dce632a5aa69b21111df2380e7d74275ade
JoaoHypo <jguibarboza@gmail.com>
2023-08-02 13:22:21 -0300

    patched json prob

--- ExtractingDataEx/simpleJsonSample.py
+++ ExtractingDataEx/simpleJsonSample.py
@@ -1,17 +1,19 @@
-import json 
+import json
+
 data = '''{
-    "name : "Hypo",
-    "Phone" : {
-        "type" : "intl",
-        "number" : "+1 734 303 4456"
-        },
-    "email" : {
-        "hide" : "yes"
+    "name": "Hypo",
+    "Phone": {
+        "type": "intl",
+        "number": "+1 734 303 4456"
+    },
+    "email": {
+        "hide": "yes"
     }
 }'''
 
 info = json.loads(data)
 print(info)
+print(type(info))
 print("--------------------------------------------")
-print('Name:',info["name"])
-print("Hide: ",info["email"]["hide"])
+print('Name:', info["name"])
+print("Hide: ", info["email"]["hide"])
